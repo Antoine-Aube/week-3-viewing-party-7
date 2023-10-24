a1ab557b1cc3a738b59cf79ddecdb45981f045bc
Antoine Aube <aaube3@gmail.com>
2023-10-24 13:14:17 -0600

    feature: authentication - cannot create a viewing party unless logged in

--- app/controllers/viewing_parties_controller.rb
+++ app/controllers/viewing_parties_controller.rb
@@ -1,4 +1,6 @@
 class ViewingPartiesController < ApplicationController 
+  before_action :require_login, only: [:new, :create]
+  
   def new
     @user = User.find(params[:user_id])
     @movie = Movie.find(params[:movie_id])
@@ -15,4 +17,15 @@ class ViewingPartiesController < ApplicationController
   def viewing_party_params 
     params.permit(:movie_id, :duration, :date, :time)
   end 
+
+  def require_login
+    unless logged_in?
+      flash[:error] = "You must be logged in to access this page"
+      redirect_to "/users/#{params[:user_id]}/movies/#{params[:movie_id]}"
+    end
+  end
+
+  def logged_in?
+    session[:user_id] != nil
+  end
 end 
